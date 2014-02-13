dadaad13c008ef8a6752144232cc540b7886ef93
stanko75 <stanko@milosev.com>
2014-02-14 00:05:16 +0100

    Sort of a separation into files and added MySql method (not implemented)

---
+++ ReadXmlTreeViewAutofac/Documentation/Interfaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadXmlTreeViewAutofac.Documentation
+{
+  public interface IMyTree
+  {
+    List<string> Read(string myFile);
+  }
+  public interface IReadXMLMyTree
+  {
+    List<string> ReadXML();
+    List<string> ReadMySql();
+  }
+}
---
+++ ReadXmlTreeViewAutofac/Documentation/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadXmlTreeViewAutofac.Documentation
+{
+  public class MyXmlReader : IReadXMLMyTree
+  {
+    private IMyTree _output;
+    public MyXmlReader(IMyTree output)
+    {
+      this._output = output;
+    }
+
+    public List<string> ReadXML()
+    {
+      return this._output.Read(@"somePath");
+    }
+
+    public List<string> ReadMySql()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
