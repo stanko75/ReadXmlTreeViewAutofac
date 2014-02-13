9aafa988082157dc8eb43fd97db68c19e561e43b
stanko75 <stanko@milosev.com>
2014-02-13 13:15:15 +0100

    XML and first version of IoC class

---
+++ ReadXmlTreeViewAutofac/ViewModel/ReadXmlTreeViewAutofacViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace ReadXmlTreeViewAutofac.ViewModel
+{
+
+  public interface IRead
+  {
+    List<string> Read();
+  }
+
+  public interface IReadXml
+  {
+    void ReadXml(string XmlFile);
+  }
+
+  public class ReadXmlImpl : IReadXml
+  {
+    private IRead _read;
+
+    public ReadXmlImpl(IRead read)
+    {
+      this._read = read;
+    }
+
+    public void ReadXml(string XmlFile)
+    {
+      this._read.Read();
+    }
+  }
+
+  public class MyReadXml : IRead
+  {
+    public List<string> Read()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
